68aaf8024088555734e8c59d36bca8ed11fa1e4c
nghia <nghiabt@mageplaza.com>
2018-07-30 16:21:03 +0700

    commit code

--- Controller/Adminhtml/Dashboard/RefreshStatistics.php
+++ Controller/Adminhtml/Dashboard/RefreshStatistics.php
@@ -1,12 +1,32 @@
 <?php
 /**
- * Copyright © 2013-2017 Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_Reports
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
  */
 
 namespace Mageplaza\Reports\Controller\Adminhtml\Dashboard;
+
 use Magento\Backend\Controller\Adminhtml\Dashboard\RefreshStatistics as AbstractClass;
 
+/**
+ * Class RefreshStatistics
+ * @package Mageplaza\Reports\Controller\Adminhtml\Dashboard
+ */
 class RefreshStatistics extends AbstractClass
 {
 }
