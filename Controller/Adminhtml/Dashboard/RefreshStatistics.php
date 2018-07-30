7cba906d542d16c81b6f509a4c3b3551ba643467
nghia <nghiabt@mageplaza.com>
2018-07-30 16:18:04 +0700

    fix bug reload Data

---
+++ Controller/Adminhtml/Dashboard/RefreshStatistics.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Mageplaza\Reports\Controller\Adminhtml\Dashboard;
+use Magento\Backend\Controller\Adminhtml\Dashboard\RefreshStatistics as AbstractClass;
+
+class RefreshStatistics extends AbstractClass
+{
+}
