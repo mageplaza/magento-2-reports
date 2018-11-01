4901d0e233d6e6c79cce85f46412add98f86024d
haitv282 <haitv282@gmail.com>
2018-11-01 09:23:24 +0700

    Fixed menu error on Magento < 2.2.4

---
+++ Plugin/MoveMenu.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_Core
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+
+namespace Mageplaza\Reports\Plugin;
+
+use Magento\Backend\Model\Menu\Builder\AbstractCommand;
+use Mageplaza\Reports\Helper\Data;
+
+/**
+ * Class MoveMenu
+ * @package Mageplaza\Core\Plugin
+ */
+class MoveMenu
+{
+    /**
+     * @var Data
+     */
+    protected $helper;
+
+    /**
+     * MoveMenu constructor.
+     * @param Data $helper
+     */
+    public function __construct(Data $helper)
+    {
+        $this->helper = $helper;
+    }
+
+    /**
+     * @param AbstractCommand $subject
+     * @param $itemParams
+     * @return mixed
+     */
+    public function afterExecute(AbstractCommand $subject, $itemParams)
+    {
+        if ($itemParams['id'] == 'Mageplaza_Reports::dashboard' && !$this->helper->isEnabled()) {
+            $itemParams['removed'] = true;
+        }
+
+        return $itemParams;
+    }
+}
