0fc94ba746269e421540841d6c092be13ba5617e
btn114 <nghiabt@mageplaza.com>
2019-01-20 02:04:17 +0700

    sales by hours card, sales by weekday cart, user wishlist

--- view/base/web/js/dashboard/initChart.js
+++ view/base/web/js/dashboard/initChart.js
@@ -35,96 +35,110 @@ define([
             var ctx = $('#' + this.options.chartData.name + '-chart');
             var data = {
                 type: 'line',
-                data: {
-                    index: this.options.chartData.name,
-                    labels: this.options.chartData['labels'],
-                    yUnit: this.options.chartData['yUnit'],
-                    datasets: [
-                        {
-                            labels: this.options.chartData['data']['labels'],
-                            label: this.options.chartData['label'][0],
-                            data: this.options.chartData['data']['data'],
-                            backgroundColor: '#977bca',
-                            borderColor: '#6f42c1',
-                            borderWidth: 1,
-                            lineTension: 0.5, //cong
-                            pointBorderWidth: 1,
-                            pointBorderColor: 'transparent',
-                            fill: this.options.chartData.isFill
-                        }
-                    ]
-                },
-                options: {
-                    fillColor: 'rgba(220,220,220,0.9)',
-                    legend: {
-                        display: true,
-
-                        position: 'bottom',
-                        labels: {
-                            usePointStyle: true,
-                            fontColor: '#333'
-                        }
-                    },
-                    tooltips: {
-                        mode: 'index',
-                        intersect: true,
-                        callbacks: {
-                            label: function (tooltipItem, data) {
-                                var dataset = data.datasets[tooltipItem.datasetIndex];
-                                var index = tooltipItem.index;
-
-                                return dataset.labels[index] + ': ' + (data.yUnit ? priceUtils.formatPrice(dataset.data[index], data.yUnit) : dataset.data[index]);
-                            },
-                            title: function (tooltipItems, data) {
-                                if (data.index === 'repeatCustomerRate') {
-                                    return tooltipItems[0].xLabel;
-                                }
-                                return '';
-                            }
-                        }
-                    },
-                    scales: {
-                        xAxes: [{
-                            display: true,
-                            labelString: 'Time',
-                            type: 'time',
-                            // distribution: 'series',
-                            time: {
-                                stepSize: this.options.chartData.stepSize,
-                                unit: 'day',
-                                displayFormats: {
-                                    day: 'Y-MMM-D'
-                                }
-                            },
-                            ticks: {
-                                callback: function (label, index, labels) {
-                                    return label;
-                                }
-                            }
-                        }],
-                        yAxes: [{
-                            scaleStartValue: 0,
-                            scaleLabel: {
-                                display: true,
-                                labelUnit: this.options.chartData.yUnit,
-                                labelString: this.options.chartData.yLabel,
-                                scaleStepWidth: 1
-                            },
-                            ticks: {
-                                min: 0,
-                                // Include a currency sign in the ticks
-                                callback: function (value, index, values) {
-                                    if (Math.floor(value) === value) {
-                                        return self.options.chartData.yUnit ? priceUtils.formatPrice(value, self.options.chartData.yUnit) : value;
-                                    }
-                                }
-                            }
-                        }]
-                    }
-                }
+                data: this.getData(),
+                options: this.getOptions()
             };
 
-            var compareDataset = {
+            new Chart(ctx, data);
+        },
+        getOptions: function () {
+            return {
+                fillColor: 'rgba(220,220,220,0.9)',
+                legend: {
+                    display: true,
+
+                    position: 'bottom',
+                    labels: {
+                        usePointStyle: true,
+                        fontColor: '#333'
+                    }
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: true,
+                    callbacks: {
+                        label: function (tooltipItem, data) {
+                            var dataset = data.datasets[tooltipItem.datasetIndex];
+                            var index = tooltipItem.index;
+                            return dataset.labels[index] + ': ' + (data.yUnit ? priceUtils.formatPrice(dataset.data[index], data.yUnit) : dataset.data[index]);
+                        },
+                        title: function (tooltipItems, data) {
+                            if (data.index === 'repeatCustomerRate') {
+                                return tooltipItems[0].xLabel;
+                            }
+                            return '';
+                        }
+                    }
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        labelString: 'Time',
+                        type: 'time',
+                        // distribution: 'series',
+                        time: {
+                            stepSize: this.options.chartData.stepSize,
+                            unit: 'day',
+                            displayFormats: {
+                                day: 'Y-MMM-D'
+                            }
+                        },
+                        ticks: {
+                            callback: function (label, index, labels) {
+                                return label;
+                            }
+                        }
+                    }],
+                    yAxes: [{
+                        scaleStartValue: 0,
+                        scaleLabel: {
+                            display: true,
+                            labelUnit: this.options.chartData.yUnit,
+                            labelString: this.options.chartData.yLabel,
+                            scaleStepWidth: 1
+                        },
+                        ticks: {
+                            min: 0,
+                            // Include a currency sign in the ticks
+                            callback: function (value, index, values) {
+                                if (Math.floor(value) === value) {
+                                    return self.options.chartData.yUnit ? priceUtils.formatPrice(value, self.options.chartData.yUnit) : value;
+                                }
+                            }
+                        }
+                    }]
+                }
+            };
+        },
+        getData: function () {
+            var data = {
+                index: this.options.chartData.name,
+                labels: this.options.chartData['labels'],
+                yUnit: this.options.chartData['yUnit'],
+                datasets: [
+                    {
+                        labels: this.options.chartData['data']['labels'],
+                        label: this.options.chartData['label'][0],
+                        data: this.options.chartData['data']['data'],
+                        backgroundColor: '#977bca',
+                        borderColor: '#6f42c1',
+                        borderWidth: 1,
+                        lineTension: 0.5, //cong
+                        pointBorderWidth: 1,
+                        pointBorderColor: 'transparent',
+                        fill: this.options.chartData.isFill
+                    }
+                ]
+            };
+            var compareDataset = this.getCompareDataSet();
+            if (this.options.chartData.isCompare === '1' || this.options.chartData.name === 'repeatCustomerRate') {
+                data.datasets.push(compareDataset);
+            }
+
+            return data;
+        },
+        getCompareDataSet: function () {
+            return {
                 labels: this.options.chartData['compareData']['labels'],
                 label: this.options.chartData['label'][1],
                 data: this.options.chartData['compareData']['data'],
@@ -137,10 +151,6 @@ define([
                 // pointHoverBackgroundColor: 'blue',
                 fill: this.options.chartData.isFill,
             };
-            if (this.options.chartData.isCompare == 1 || this.options.chartData.name === 'repeatCustomerRate') {
-                data.data.datasets.push(compareDataset);
-            }
-            new Chart(ctx, data);
         }
     });
 
