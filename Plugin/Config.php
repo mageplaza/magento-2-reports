a97a086a37c2a12fc9c2e760c89358825ce0533a
eric <thainc@mageplaza.com>
2019-04-18 09:37:48 +0700

    fix bug redirect to dashboard Magento 2.2.8

---
+++ Plugin/Config.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_Reports
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+
+namespace Mageplaza\Reports\Plugin;
+
+use Magento\Framework\App\Route\Config as BackendConfig;
+use Mageplaza\Reports\Helper\Data;
+
+/**
+ * Class Config
+ * @package Mageplaza\Reports\Plugin
+ */
+class Config
+{
+    /**
+     * @var Data
+     */
+    protected $helper;
+
+    /**
+     * MoveMenu constructor.
+     *
+     * @param Data $helper
+     */
+    public function __construct(Data $helper)
+    {
+        $this->helper = $helper;
+    }
+
+    /**
+     * @param BackendConfig $subject
+     * @param $result
+     * @param $frontName
+     * @param null $scope
+     * @return mixed
+     */
+    public function afterGetRouteByFrontName(BackendConfig $subject, $result, $frontName, $scope = null)
+    {
+        if ($this->helper->versionCompare('2.2.8','=')){
+            if (!$result) {
+                return $frontName;
+            }
+        }
+        return $result;
+    }
+}
